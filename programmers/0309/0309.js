7539af03f0dec68c738e0aae192921800364b328
leemanjae02 <honthalg@gmail.com>
2026-03-15 21:36:40 +0900

    폴더 생성 및 문제 풀이 기본 구조 작성

---
+++ programmers/0309/0309.js
@@ -0,0 +1,10 @@
+let participant = ["lee", "kim", "park"];
+let completion = ["lee", "kim"];
+
+function solution(participant, completion) {
+  var answer = "";
+
+  return answer;
+}
+
+console.log(solution(participant, completion));
